0eb98a8783a2fcb6062b165a1eb38527cc88316a
AoiTuNa <bestspark123@naver.com>
2024-08-14 17:55:40 +0900

    fest/ 백준 10926번 해결

---
+++ ConsoleApp1/10926/10926.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1._10926
+{
+    public class _10926
+    {
+        public String SurpriseManufacturing(String word)
+        {
+            return word + "??!";
+        }
+    }
+}
---
+++ ConsoleApp1/10926/10926Test.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1._10926
+{
+    [TestFixture]
+    public class _10926Test
+    {
+        private _10926 _10926;
+        [SetUp]
+        public void Setup()
+        {
+            _10926 = new _10926();
+        }
+        [Test]
+        public void test1()
+        {
+            String result = _10926.SurpriseManufacturing("joonas");
+            ClassicAssert.AreEqual("joonas??!", result);
+        }
+        [Test]
+        public void test2()
+        {
+            String result = _10926.SurpriseManufacturing("baekjoon");
+            ClassicAssert.AreEqual("baekjoon??!", result);
+        }
+    }
+}
