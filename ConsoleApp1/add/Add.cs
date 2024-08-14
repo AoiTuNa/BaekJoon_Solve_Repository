2a9bf3474ba0fe5590d337dff5e040c8405c76b0
AoiTuNa <bestspark123@naver.com>
2024-08-14 17:33:56 +0900

    feat/ add기능 및 nunit 테스트 연습

---
+++ ConsoleApp1/add/Add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.add
+{
+    public class Add
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ConsoleApp1/add/AddTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+
+namespace ConsoleApp1.add
+{
+    [TestFixture]
+    public class AddTest
+    {
+        private Add _add;
+        [SetUp]
+        public void Setup()
+        {
+            _add = new Add();
+        }
+        [Test]
+        public void Add_TwoPositiveNumbers_ReturnsCorrectSum()
+        {
+            int result = _add.add(3, 4);
+            ClassicAssert.AreEqual(7, result);
+        }
+    }
+}
+
